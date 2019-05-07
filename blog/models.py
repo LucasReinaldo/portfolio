a253823433dee0943bf2ac4ad74763cd28a15acb
LucasReinaldo <lucasreinaldo.demelo@hotmail.com>
2019-05-07 15:51:18 +0100

    Blog improvements and static files

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+
+    # Troca o nome do title em /admin para o nome do title do post.
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200] + '[...]'
+
