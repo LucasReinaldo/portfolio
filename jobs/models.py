bbe366fff026c73a1ffa4107b35527343716cf1e
Lucas Reinaldo de Melo <lucasreinaldo.demelo@hotmail.com>
2019-08-23 13:07:57 +0100

    modal popup

--- jobs/models.py
+++ jobs/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 
 class Job(models.Model):
     title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=800)
+    challenge = models.CharField(max_length=500)
+    team = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     pub_date = models.DateField(default=timezone.now)
     site = models.CharField(max_length=100, default='https://')
