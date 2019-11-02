1c7e0a51a2a4044a31dc7c695c19f5ebdc61a96e
Lucas Reinaldo de Melo <lucasreinaldo.demelo@hotmail.com>
2019-11-02 16:37:19 +0000

    requirements

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.utils import timezone
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Job
 
+
 def home(request):
     jobs = Job.objects.all().order_by('-pub_date')
     return render(request, 'jobs/home.html', {'jobs':jobs})
